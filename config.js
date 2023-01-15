23e822715379f8d76b4ebe2744ed76415f2c17e7
awal-cendana <awal-cendana1@outlook.com>
2023-01-15 13:18:55 +0800

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  API_KEY_RM_BG: "FxeExuMpHGGsxsTj27wKmkyw",
+  // API_KEY_OPEN_AI: "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",
+  API_KEY_OPEN_AI: "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",
+};
